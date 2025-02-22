cc62f51dced5474abfff1241f354cf0640c13f17
Sanjay Ragavendar <91368803+SanjayRagavendar@users.noreply.github.com>
2025-02-22 00:58:24 +0000

    rm backend

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import config from "./src/utils/config.js";
-import cors from "cors";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.listen(config.PORT, () =>
-  console.log(`Server listening on port ${config.PORT}`)
-);
